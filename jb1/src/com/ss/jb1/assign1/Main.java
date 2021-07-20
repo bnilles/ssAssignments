a8192f04d2bd6ec3d7ef5ec1b881fe7e9511c05b
bnilles <joseph.nilles@substack.com>
2021-07-19 22:00:52 -0700

    Finished part 1

--- jb1/src/com/ss/jb1/assign1/Main.java
+++ jb1/src/com/ss/jb1/assign1/Main.java
@@ -14,6 +14,9 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Pattern p = new Pattern();
+		p.printAllParts();
+		
 
 	}
 
--- jb1/src/com/ss/jb1/assign1/Pattern.java
+++ jb1/src/com/ss/jb1/assign1/Pattern.java
@@ -9,7 +9,49 @@ package com.ss.jb1.assign1;
  */
 public class Pattern {
 	public void printPart1() {
-		System.out.println();
+		System.out.println("1)");
+		for(int i = 0; i < 4; i++) {
+			System.out.print("*".repeat(i + 1));
+			System.out.println();
+		}
+		System.out.println(".........");
+	}
+	
+	public void printPart2() {
+		System.out.println("2)");
+		System.out.println("..........");
+		for(int i = 3; i >= 0; i--) {
+			System.out.print("*".repeat(i + 1));
+			System.out.println();
+		}
+	}
+	
+	public void printPart3() {
+		System.out.println("3)");
+		for(int i = 0; i < 4; i++) {
+			System.out.print(" ".repeat(5 - i));
+			System.out.print("*".repeat(2 * i + 1));
+			System.out.println();
+		}
+		System.out.println("...........");
+	}
+	
+	public void printPart4() {
+		System.out.println("4)");
+		System.out.println("............");
+		for(int i = 3; i >= 0; i--) {
+			System.out.print(" ".repeat(5 - i));
+			System.out.print("*".repeat(2 * i + 1));
+			System.out.println();
+		}
+	}
+	
+	public void printAllParts() {
+		this.printPart1();
+		this.printPart2();
+		this.printPart3();
+		this.printPart4();
 	}
 
 }
+
