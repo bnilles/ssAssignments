64d22f66c660e3602abdc325634851faaec26a39
bnilles <joseph.nilles@substack.com>
2021-07-19 21:14:17 -0700

    Initial commit for Day one assign 1

---
+++ jb1/src/com/ss/jb1/assign1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ss.jb1.assign1;
+
+/**
+ * @author bnilles
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ jb1/src/com/ss/jb1/assign1/Pattern.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.ss.jb1.assign1;
+
+/**
+ * @author bnilles
+ *
+ */
+public class Pattern {
+	public void printPart1() {
+		System.out.println();
+	}
+
+}
