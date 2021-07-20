d568f1e4b1530cb850b1cbec607d1099c9bdd105
bnilles <joseph.nilles@substack.com>
2021-07-19 22:02:28 -0700

    add part 2 initial

---
+++ jb1/src/com/ss/jb1/assign2/Guess.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.ss.jb1.assign2;
+
+/**
+ * @author bnilles
+ *
+ */
+public class Guess {
+
+}
---
+++ jb1/src/com/ss/jb1/assign2/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ss.jb1.assign2;
+
+/**
+ * @author bnilles
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
