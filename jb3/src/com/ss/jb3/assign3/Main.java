52c1274c5c9d6bc5cb2d3fc81cefd3b59bfa8706
bnilles <joseph.nilles@substack.com>
2021-07-22 08:36:28 -0700

    fixed assign 3 to read from console

--- jb3/src/com/ss/jb3/assign3/Main.java
+++ jb3/src/com/ss/jb3/assign3/Main.java
@@ -3,6 +3,8 @@
  */
 package com.ss.jb3.assign3;
 
+import java.util.Scanner;
+
 /**
  * @author bnilles
  *
@@ -16,7 +18,21 @@ public class Main {
 		// TODO Auto-generated method stub
 		char checkChar = 'x';
 		CharCounter cc = new CharCounter();
-		System.out.println(cc.countCharsInFile("src\\assign3.txt", checkChar));
+		try {
+			Scanner scanner = new Scanner(System.in);
+			String input = "";
+			System.out.println("enter run to a char to check the count or \"exit\" to leave.");
+			input = scanner.nextLine();
+			while (input.equals("exit") == false) {
+				System.out.println(cc.countCharsInFile("src\\assign3.txt", input.charAt(0)));
+				System.out.println("enter run to a char to check the count or \"exit\" to leave.");
+				input = scanner.nextLine();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("there was an input error");
+		}
+		
 
 	}
 
