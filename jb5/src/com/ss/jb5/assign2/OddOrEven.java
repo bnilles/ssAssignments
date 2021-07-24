ed8008f413ed826f4510e27255ddbe74d12f8a9d
bnilles <joseph.nilles@substack.com>
2021-07-24 14:39:47 -0700

    assign 2 completed

---
+++ jb5/src/com/ss/jb5/assign2/OddOrEven.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.ss.jb5.assign2;
+
+import java.util.StringJoiner;
+
+/**
+ * @author bnilles
+ *
+ */
+public class OddOrEven {
+
+	/**
+	 * @param args
+	 */
+	
+	public String checkOddOrEven  (Integer[] integers ) {
+		StringJoiner joinNumbers = new StringJoiner(",");
+		for (Integer integer : integers) {
+			if(integer % 2  == 0) {
+				joinNumbers.add("e" + integer);
+			} else {
+				joinNumbers.add("o" + integer);
+			}
+		}
+		return joinNumbers.toString();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		OddOrEven ooe = new OddOrEven();
+		Integer[] integers  = {1,2,34,4,5,23,32,2,3,0};
+		System.out.println(ooe.checkOddOrEven(integers));
+	}
+
+}
